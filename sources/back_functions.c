66c537f4c610ac7481eac781957a33279595b10e
Eric García Castro <ericgar2003@gmail.com>
2024-04-18 22:20:53 +0200

    carregar_amistats() ACABADO

--- sources/back_functions.c
+++ sources/back_functions.c
@@ -13,7 +13,7 @@ short obtenir_usuaris(char *argv[], short n_elem)
     return (id); // Retorna el valor passat com a paràmetre en la trucada a l'executable convertit a enter o -1 en cas de no existir l'id
 }
 
-bool carregar_amistats(int *amistats)
+bool carregar_amistats(int **amistats)
 {
     bool resultat = true;
     short i, j, fila_columna;
@@ -26,13 +26,13 @@ bool carregar_amistats(int *amistats)
     else
     {
         fscanf(fitxer, "%hd", &fila_columna);
-        amistats = malloc(sizeof(short) * fila_columna * fila_columna); // Reserva memòria pel vector amistats de forma dinàmica
+        *amistats = malloc(sizeof(int) * fila_columna * fila_columna); // Reserva memòria pel vector amistats de forma dinàmica
 
         for (i = 0; i < fila_columna; i++)
         {
             for (j = 0; j < fila_columna; j++)
             {
-                fscanf(fitxer, "%hd", &amistats[i * fila_columna + j]); // Adreçament a dinàmic a memòria on amistats[fila * NC + columna]
+                fscanf(fitxer, "%d", &(*amistats)[i * fila_columna + j]); // Adreçament a dinàmic a memòria on amistats[fila * NC + columna]
             }
         }
         fclose(fitxer);
