20cfb7cd062a39c9d6894e6e304bb30a4368adb7
ivanmoliinero <imolimort@gmail.com>
2024-05-26 17:15:59 +0200

    versio final

--- sources/back_functions.c
+++ sources/back_functions.c
@@ -483,9 +483,9 @@ void afegir_usuaris_main(persona_t **usuaris, char **amistats, short *n_usuaris,
 bool nomes_caracters(char desti[])
 {
     bool nomes_lletres = true;
-    for (int i = 0; desti[i] != '\n' && nomes_lletres; i++)
+    for (int i = 0; desti[i] != '\0' && nomes_lletres; i++)
     {
-        if (('a' > desti[i] || desti[i] > 'z') && ('A' > desti[i] || desti[i] > 'Z') && (desti[i] != ' '))
+        if (('a' > desti[i] || desti[i] > 'z') && ('A' > desti[i] || desti[i] > 'Z') && (desti[i] != ' ') && (desti[i] != '\n'))
             nomes_lletres = false;
     }
     return nomes_lletres;
